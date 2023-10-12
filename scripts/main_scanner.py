b0716eba8887b784569126e6d8a51e052a246d09
piyushmalik865@gmail.com <piyushmalik865@gmail.com>
2023-10-12 16:46:39 -0400

    second commit

--- scripts/main_scanner.py
+++
@@ -1,3 +0,0 @@
-import subdomain_scanner
-
-subdomain_scanner.scanner(input("Please Enter the Domain you want to scan : "),input("Enter the alteration file for subdomains : "), input("Enter the File for operating Protocols : "))
